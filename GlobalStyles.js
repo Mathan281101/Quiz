7d68100a64102a1a55f1d48c376494069a0237f0
Mathan281101 <Mathan281101>
2023-12-13 07:11:54 +0000

    Pushed from Locofy Builder

---
+++ GlobalStyles.js
@@ -0,0 +1,30 @@
+/* fonts */
+export const FontFamily = {
+  notoSansBold: "NotoSans-Bold",
+  interRegular: "Inter-Regular",
+  interBold: "Inter-Bold",
+  notoSansRegular: "NotoSans-Regular",
+  nunitoSans12ptRegular: "NunitoSans12pt-Regular",
+};
+/* font sizes */
+export const FontSize = {
+  size_xs: 12,
+  size_mini: 15,
+  size_2xs: 11,
+  size_lg: 18,
+};
+/* Colors */
+export const Color = {
+  colorGhostwhite: "#f6faff",
+  colorDarkslategray_100: "#393d46",
+  colorDarkslategray_200: "rgba(57, 61, 70, 0.5)",
+  colorMediumseagreen: "#2ecc71",
+  colorBlack: "#000",
+  colorGainsboro: "#d9d9d9",
+  colorWhite: "#fff",
+  colorDeepskyblue: "#08b3f1",
+};
+/* border radiuses */
+export const Border = {
+  br_8xs: 5,
+};
